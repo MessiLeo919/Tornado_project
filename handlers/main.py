68efea15b7beb6f15e4430e34c5d03b99e4a372f
Messi <690995749@qq.com>
2018-06-23 10:25:46 +0800

    增加缩略图

--- handlers/main.py
+++ handlers/main.py
@@ -10,8 +10,7 @@ class IndexHandler(tornado.web.RequestHandler):
     def get(self, *args, **kwargs):
         images_path = os.path.join(self.settings.get('static_path'),'uploads')
         # print(self.settings.get('static_path'))
-        images = photo.get_images(images_path)
-        print(images)
+        images = photo.get_images(images_path)   #改目录下图片文件组成的列表，含路径
         self.render('index.html',
                     images=images)
 
@@ -20,18 +19,11 @@ class ExploreHandler(tornado.web.RequestHandler):
     Explore page, photo of other users.
     """
     def get(self, *args, **kwargs):
-        images_path = os.path.join(self.settings.get('static_path'), 'uploads')
-        # print("images_path---{}".format(images_path))
-        images = photo.get_images(images_path)
+        thumbs_path = os.path.join(self.settings.get('static_path'), 'uploads/thumbs')
+        thumbs_images = photo.get_images(thumbs_path)
         # print("images-----",images)
-        for i in images:
-            photo.get_thumbnail(i)
-        thumbnail_path = os.path.join(self.settings.get('static_path'), 'uploads/thumbs')
-        print("thumbnail_path---",thumbnail_path)
-        thumbsimages = photo.get_images(thumbnail_path)
-        print(thumbsimages)
         self.render('explore.html',
-                    thumbsimages=thumbsimages)
+                    thumbsimages=thumbs_images)
 
 class PostHandler(tornado.web.RequestHandler):
     """
@@ -42,20 +34,29 @@ class PostHandler(tornado.web.RequestHandler):
     def get(self, post_id):
         self.render('post.html', post_idd=post_id)
 
+
+
+
 class UploadHandler(tornado.web.RequestHandler):
     '''
     接收文件上传
     '''
     def get(self, *args, **kwargs):
         self.render('upload.html')
+
     def post(self, *args, **kwargs):
         img_files = self.request.files.get('newimg',None)  #得到的是列表
-        # print("img_files{}".format(img_files))
-        print("img_files-----------",type(img_files))
+        # print(img_files)
         for img_file in img_files:
+            # print(img_file)
             with open('./static/uploads/'+img_file['filename'],'wb') as f:
                 f.write(img_file['body'])
-                print("img_file{}".format(img_file))
+                # print("img_file{}".format(img_file))
             self.write({'got file': img_file['filename']})
+
+            pic_path = os.path.join(self.settings.get('static_path'),'uploads/'+img_file['filename'])
+            print("pic_path----",pic_path)
+
+            photo.make_thumb(pic_path)
 
 
